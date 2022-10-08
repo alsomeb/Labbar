506c26fa2908fd5406f025dfe4fd9089017314af
alsomeb <alsomeb@gmail.com>
2022-10-08 14:53:36 +0200

    Created tests for Figure Factory

--- test/uppgift7ab/FigureFactoryTest.java
+++ test/uppgift7ab/FigureFactoryTest.java
@@ -16,7 +16,7 @@ class FigureFactoryTest {
         assertEquals(expected.getClass().getSimpleName(), actual.getClass().getSimpleName()); // har samma klass namn
         assertInstanceOf(Circle.class, actual); // är en instans av Circle
 
-        // samma som ovan fast utan JUnits hjälp metod för den har inte en NOT metod, kan nog bygga liknande med assertTrue/False och assertEquals/NotEquals
+        // samma Som ovan fast utan JUnits hjälp metod för den har inte en NOT metod, kan nog bygga liknande med assertTrue/False och assertEquals/NotEquals
         assert(!(actual instanceof LikeSidedTriangle));
         assert(!(actual instanceof Rectangle));
     }
